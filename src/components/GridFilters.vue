7aed0a54b69727c44af0444daac1b999dabbc292
Ryan Mulligan <rmulligan@elevatedthird.com>
2018-03-18 17:43:36 -0700

    Add foundation for testing a simple track

---
+++ src/components/GridFilters.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :id="id" class="grid-filters">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GridFilters',
+  components: {},
+  
+  data() {
+    return {
+      id: 'filters'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
