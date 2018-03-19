ce91e7ba39c6ec1ba966f7c44f039b371336c497
Ryan Mulligan <rmulligan@elevatedthird.com>
2018-03-18 17:12:13 -0700

    Move codepen files into prototype directory and init vue project with component files

---
+++ src/components/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello world!</h1>
+</template>
+
+<script>
+import GridTrack from './GridTrack'
+
+export default {
+  name: 'Grid',
+  components: {
+    GridTrack
+  }
+}
+</script>
+
+<style>
+
+</style>
